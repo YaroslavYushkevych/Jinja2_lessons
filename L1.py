eab819fcd45f91dec49c9581c968c0f575b82227
YaroslavYushkevych <yaroslavyushkevych@gmail.com>
2023-05-04 19:22:09 +0300

    Update L1 - Finished#1

--- L1.py
+++ L1.py
@@ -1,8 +1,61 @@
 from jinja2 import Template
 
-name = "Yaroslav"
+def temp_1(name = "Yaroslav", age = 22):
+    tm = Template("I`m {{ a }} old & my name {{ n }}.")
+    return tm.render(n = name, a = age)
 
-tm = Template("Hello {{ name }}")
-msg = tm.render(name=name)
+def temp_2(name = "Yaroslav", age = 22):
+    tm = Template("I`m {{ a*2 }} old & my name {{ n.upper() }}.")
+    return tm.render(n = name, a = age)
 
-print(msg)
+def temp_6():
+    per = {'name': 'Yaroslav', 'age': 22}
+    tm = Template("I`m {{p.age}} old & my name {{p.name}}.")
+    return tm.render(p = per)
+
+def temp_7():
+    per = {'name': 'Yaroslav', 'age': 22}
+    tm = Template("I`m {{p['age']}} old & my name {{p['name']}}.")
+    return tm.render(p = per)
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def temp_3(self):
+        tm = Template("I`m {{ a }} old & my name {{ n }}.")
+        return tm.render(n = self.name, a = self.age)
+
+    def getName(self):
+        return self.name
+
+    def getAge(self):
+        return self.age
+
+
+#Standart temp - like f-strings
+print("Temp_1: ", temp_1())
+
+#Temp & somthing operations method
+print("Temp_2: ", temp_2())
+
+#Temp & class method
+per = Person("Yaroslav", 22)
+print("Temp_3: ", per.temp_3())
+
+#OR other Temp & class method
+tm = Template("I`m {{ p.age }} old & my name {{ p.name }}.")
+msg = tm.render(p=per)
+print("Temp_4: ", msg)
+
+#Use getters & setters class method
+tm = Template("I`m {{p.getAge()}} old & my name {{ p.getName() }}.")
+msg = tm.render(p=per)
+print("Temp_5: ", msg)
+
+#Use list for temp version 1
+print("Temp_6: ", temp_6())
+
+#Use list for temp version 2
+print("Temp_7: ", temp_7())
